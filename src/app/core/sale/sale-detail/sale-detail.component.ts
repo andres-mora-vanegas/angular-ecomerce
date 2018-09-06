50eaea111582423fbb0ccbbac1633ef09201c8f1
DESKTOP-0E6HK9S\andypc <andresmoradeveloper@gmail.com>
2018-09-05 20:15:21 -0500

    [update] add dto table

--- src/app/core/sale/sale-detail/sale-detail.component.ts
+++ src/app/core/sale/sale-detail/sale-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SaleDto } from '../sale.dto';
 
 @Component({
   selector: 'app-sale-detail',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaleDetailComponent implements OnInit {
 
+  @Input()
+  arrSaleDto: Array<SaleDto>;
   constructor() { }
 
   ngOnInit() {
